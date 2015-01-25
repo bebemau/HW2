f055024552c80fd1a6f24ccd52f224df30dfdad3
keren man <kerenman@MacBook-Pro.hsd1.wa.comcast.net>
2015-01-24 19:38:19 -0800

    Initial Commit

---
+++ HW2/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  HW2
+//
+//  Created by keren man on 1/24/15.
+//  Copyright (c) 2015 keren man. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ HW2/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  HW2
+//
+//  Created by keren man on 1/24/15.
+//  Copyright (c) 2015 keren man. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
